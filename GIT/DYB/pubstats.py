2d4517d2febc38287bcd1c3b3c0f2e6f596cf672
David Jaffe <djaffe@bnl.gov>
2018-10-16 20:20:36 -0400

    additional figures for Oct 2018 meeting

--- GIT/DYB/pubstats.py
+++ GIT/DYB/pubstats.py
@@ -7,7 +7,7 @@ daya bay publication stats
 '''
 import math
 import sys
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 class pubstats():
@@ -57,6 +57,70 @@ class pubstats():
                        }
         
         return
+    def timetosub(self):
+        '''
+        histogram of the time taken to submit to a journal
+        '''
+        plt.rcdefaults() # why?
+
+
+        fig,ax = plt.subplots()
+
+        x = []
+        for name in self.pubs:
+            a = sum(self.pubs[name][:3])
+            x.append(a)
+        nbin = 8
+        hist,bins = np.histogram(x,bins=nbin)
+
+        ax.set_yticks(np.arange(0,4))
+        ax.set_ylim([0,max(hist)*1.1])
+        ax.set_xlim([0,int(max(x)/nbin+1)*nbin])
+        ax.set_xlabel('Days to submission to journal')
+        ax.set_title(str(int(np.mean(x)))+' days to submission on average')
+
+        width = 0.7/0.9 * (bins[1] - bins[0])
+        center = (bins[:-1] + bins[1:]) / 2
+        plt.bar(center, hist, align='center', width=width)
+        plt.grid()
+
+        pdf = self.figdir + 'average_days.pdf'
+        plt.savefig(pdf)
+        print 'pubstats.timetosub Wrote',pdf
+        return
+
+    def comments(self):
+        '''
+        trend of # of comments/manuscript vs time
+        '''
+        plt.rcdefaults() # why?
+        fig,ax = plt.subplots()
+        y = [4,9,7,3,12,6,6,7,4]
+        x = np.arange(1,len(y)+1)
+        meany = np.average(y)
+
+        xmi,xma = 0.5,max(x)+0.5
+        ax.set_xlim([xmi,xma])
+        ax.set_ylim([0,max(y)*1.1])
+        ax.set_xlabel('Chronological order of submission to journal')
+        ax.set_ylabel('The number of collaborators who contributed comments')
+        ax.set_title('{0:.2f}'.format(meany)+' comments/manuscript on average')
+        plt.grid()
+
+
+        l = plt.plot(x,y, 'ro')
+        plt.setp(l, markersize=30/2)
+        plt.setp(l, markerfacecolor='red')
+
+        l = plt.plot([xmi,xma],[meany,meany],'b--')
+
+
+        pdf = self.figdir + 'trend.pdf'
+        plt.savefig(pdf)
+        print 'pubstats.trend Wrote',pdf
+        
+        return
+
     def plot(self):
         '''
         make horizontal,stacked bar chart
@@ -66,7 +130,7 @@ class pubstats():
         width = 0.5 # width of bars in bar chart
         offset = 0.1 # horiz offset of first bar from ordinate axis
         
-        left = numpy.array( range(len(self.order)) ) + offset
+        left = np.array( range(len(self.order)) ) + offset
         bottom = [0 for x in range(len(self.order))]
         p = []
         maxheight = 0
@@ -96,3 +160,5 @@ class pubstats():
 if __name__ == '__main__' :
     P = pubstats()
     P.plot()
+    P.timetosub()
+    P.comments()
